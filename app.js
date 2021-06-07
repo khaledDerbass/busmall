0aa753d2c1b69b0d872f02b8975bfc1f401d7107
Khaled <derbasskhaled1@gmail.com>
2021-06-07 04:00:04 +0300

    to push the modified class11

--- app.js
+++ app.js
@@ -1,65 +1,75 @@
 'use srtict'
-let firstImageElement=document.getElementById('first-image');
-let thirdImageElement=document.getElementById('second-image');
-let secondImageElement=document.getElementById('third-image');
 
-let maxAttempts=10;
+
+let maxAttempts=25;
 let userAttemptsCounter=0;
 
-// the random number index for the left image
+// the random number index for the first image
 let firstImageIndex; 
 
-// the random number index for the right image
-let secondImageIndex;
 // the random number index for the second image
+let secondImageIndex;
+// the random number index for the third image
 let thirdImageIndex;
 
 
+// link the DIV images elements declared in HTML with variables in JS for each by id  
+let firstImageElement=document.getElementById('first-image');
+let secondImageElement=document.getElementById('second-image');
+let thirdImageElement=document.getElementById('third-image');
+///
+let imgVotingElement=document.getElementById('imgVoting');
 
-function Goat(name,source) {
+
+
+let btnResult = document.getElementById("voteResult");
+
+
+function product(name,source) {
   this.name=name;
   this.source=source;
   this.votes=0;
 
-  Goat.allGoats.push(this);
+  product.allproducts.push(this);
 }
 
-// will contain all of the goats that will be created
+// will contain all of the products that will be created
 
-Goat.allGoats=[];
+product.allproducts=[];
 
 
-new Goat('bag','assets/bag.jpg');//0
-new Goat('banana', 'assets/banana.jpg');//1
-new Goat('bathroom', 'assets/bathroom.jpg');//2
-new Goat('boots', 'assets/boots.jpg');//3
-new Goat('breakfast', 'assets/breakfast.jpg');//4
-new Goat('bubblegum', 'assets/bubblegum.jpg');//5
-new Goat('chair', 'assets/chair.jpg');//6
-new Goat('cthulhu', 'assets/cthulhu.jpg');//7
-
-new Goat('dragon','assets/dragon.jpg');//0
-new Goat('pen', 'assets/pen.jpg');//1
-new Goat('pet-weep', 'assets/pet-weep.jpg');//2
-new Goat('scissors', 'assets/scissors.jpg');//3
-new Goat('shark', 'assets/shark.jpg');//4
-new Goat('sweep', 'assets/sweep.jpg');//5
-new Goat('tauntaun', 'assets/tauntaun.jpg');//6
-new Goat('unicorn', 'assets/unicorn.jpg');//7
-new Goat('usb', 'assets/usb.gif');//5
-new Goat('water-can', 'assets/water-can.jpg');//6
-new Goat('wine-glass', 'assets/wine-glass.jpg');//7
+new product('bag','assets/bag.jpg');//0
+new product('banana', 'assets/banana.jpg');//1
+new product('bathroom', 'assets/bathroom.jpg');//2
+new product('boots', 'assets/boots.jpg');//3
+new product('breakfast', 'assets/breakfast.jpg');//4
+new product('bubblegum', 'assets/bubblegum.jpg');//5
+new product('chair', 'assets/chair.jpg');//6
+new product('cthulhu', 'assets/cthulhu.jpg');//7
+new product('dragon','assets/dragon.jpg');//8
+new product('pen', 'assets/pen.jpg');//9
+new product('pet-sweep', 'assets/pet-sweep.jpg');//10
+new product('scissors', 'assets/scissors.jpg');//11
+new product('shark', 'assets/shark.jpg');//12
+new product('sweep', 'assets/sweep.png');//13
+new product('tauntaun', 'assets/tauntaun.jpg');//14
+new product('unicorn', 'assets/unicorn.jpg');//15
+new product('usb', 'assets/usb.gif');//16
+new product('water-can', 'assets/water-can.jpg');//17
+new product('wine-glass', 'assets/wine-glass.jpg');//18
+new product('dog-dug', 'assets/dog-dug.jpg');//19
 
 
 function generateRandomIndex() {
-  // 0 => 21
-  return Math.floor(Math.random() * Goat.allGoats.length); 
+  // 0 => 19
+  return Math.floor(Math.random() * product.allproducts.length); 
 }
 
 // console.log(generateRandomIndex());
 
 
-function renderThreeassets() {
+function renderThreeAssets(productArray) {
+  
   // first pic
   firstImageIndex=generateRandomIndex();
   // second pic
@@ -68,12 +78,30 @@ function renderThreeassets() {
   thirdImageIndex= generateRandomIndex();
 
 
+  // While there remain elements to generate
+  /*
+while (currentIndex) {
+
+  // Pick a remaining element…
+  rand = Math.floor(Math.random() * currentIndex--);
+
+  // And swap it with the current element.
+  temp = array[currentIndex];
+  array[currentIndex] = array[rand];
+  array[rand] = temp;
+}
+return array;
+*/
+
+
   while ((firstImageIndex === secondImageIndex) || (firstImageIndex === thirdImageIndex) || (secondImageIndex === thirdImageIndex)) 
   
   {
       firstImageIndex=generateRandomIndex();
       secondImageIndex=generateRandomIndex();
   }
+
+
 /*
   while((secondImageIndex !== thirdImageIndex))
   {
@@ -82,22 +110,22 @@ function renderThreeassets() {
  
 */
 
-  console.log(Goat.allGoats[firstImageIndex].source);
+  console.log(product.allproducts[firstImageIndex].source);
 
-  // make the source for the left and right image equal to the random goat source
-  firstImageElement.src=Goat.allGoats[firstImageIndex].source;
 
-  secondImageElement.src=Goat.allGoats[secondImageIndex].source;
-  thirdImageElement.src=Goat.allGoats[thirdImageIndex].source;
-  
+  // make the source for the first and second and third image equal to the random product source
+  firstImageElement.src=product.allproducts[firstImageIndex].source;
+  secondImageElement.src=product.allproducts[secondImageIndex].source;
+  thirdImageElement.src=product.allproducts[thirdImageIndex].source;
+
 }
-renderThreeassets();
+renderThreeAssets();
 
 // add event listner
 // container
-firstImageElement.addEventListener('click',handleUserClick);
-secondImageElement.addEventListener('click',handleUserClick);
-thirdImageElement.addEventListener('click',handleUserClick);
+imgVotingElement.addEventListener('click',handleUserClick);
+
+
 
 
 function handleUserClick(event) {
@@ -119,43 +147,64 @@ function handleUserClick(event) {
 
   if (userAttemptsCounter<=maxAttempts) {
 
+    btnResult.disabled = true;
 
     if (event.target.id==='first-image') {
       // the random number
-      // Goat.allGoats[5].votes++
-      Goat.allGoats[firstImageIndex].votes++
+      // product.allproducts[5].votes++
+      product.allproducts[firstImageIndex].votes++
 
     }
     else if(event.target.id==='second-image')
     {
-      Goat.allGoats[secondImageIndex].votes++
+      product.allproducts[secondImageIndex].votes++
     }
     else if(event.target.id==='third-image')
     {
-      Goat.allGoats[thirdImageIndex].votes++
+      product.allproducts[thirdImageIndex].votes++
     }
+    console.log(product.allproducts);
+    renderThreeAssets();
+    
+   }
 
-    console.log(Goat.allGoats);
-    renderThreeassets();
+    else
+    {
+      btnResult.disabled = false;
+      imgVotingElement.removeEventListener('click',UserClick);
+      
+    }
+   
+    
+  
+
+  }
 
 
-  }else{
+
+  btnResult.addEventListener('click',UserClick);
+
+
+function UserClick(event) {
+  
     // show results
+    btnResult.disabled = false;
+
+
     let list=document.getElementById('results-list');
-    for (let i = 0; i < Goat.allGoats.length; i++) {
-      let goatResult=document.createElement('li');
+    for (let i = 0; i < product.allproducts.length; i++) {
+      let productResult=document.createElement('li');
 
-      list.append(goatResult);
+      list.append(productResult);
 
-      goatResult.textContent=`${Goat.allGoats[i].name} has ${Goat.allGoats[i].votes} votes`;
+      productResult.textContent=`${product.allproducts[i].name} has ${product.allproducts[i].votes} votes`;
       
     }
 
     // stop the clicking
-    firstImageElement.removeEventListener('click',handleUserClick);
-    secondImageElement.removeEventListener('click',handleUserClick);
-    thirdImageElement.removeEventListener('click',handleUserClick);
+    btnResult.removeEventListener("click", UserClick);
+    
 
   }
 
-}
+
